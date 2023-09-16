fb9b0d7c7f268e4ce437ff5a5cb2eda334967ff4
vivekkrsk <vivekkrsk1@gmail.com>
2023-09-16 20:41:38 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'DB URL';
+mongoose.connect(dbURI,{useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => app.listen(3050))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.get('/trainers', requireAuth, (req, res) => res.render('trainers'));
+app.use(authRoutes);
